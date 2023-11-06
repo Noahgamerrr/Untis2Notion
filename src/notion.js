72e195add448516560f0b140e3c326b0cea50ada
Noahgamerrr <noah.arsic@gmx.at>
2023-11-06 21:02:06 +0100

    Table for monthly report now gets generated

--- src/notion.js
+++ src/notion.js
@@ -84,12 +84,45 @@ async function uploadTimetable(timetable) {
     await Promise.all(awaitCreation);
 }
 
-async function resetDailyGoals() {
-    const pageId = process.env.GOALS_ID;
-    const contentList = await notion.blocks.children.list({
-        block_id: pageId
-    });
-    let toDoList = contentList.results.filter(b => b.to_do?.checked);
+async function createMonthlyReportPage(database_id) {
+    const date = new Date();
+    const month = date.toLocaleString('default', {month: 'long'});
+    const year = date.getFullYear();
+    const monthNum = date.getMonth() + 1;
+    const daysInMonth = new Date(year, monthNum, 0).getDate();
+    await notion.pages.create({
+        parent: {
+            type: "database_id",
+            id: database_id
+        },
+        properties: {
+            Name: {
+                title: [
+                    {
+                        text: {
+                            content: month +" " +year
+                        }
+                    }
+                ]
+            }
+        },
+        children: [
+            {
+                object: "block",
+                table: {
+                    table_width: daysInMonth,
+                    has_column_header: true,
+                    has_row_header: true,
+                },
+                object: "block",
+                table_row
+            }
+        ]
+    })
+}
+
+async function resetDailyGoals(content) {
+    let toDoList = content.results.filter(b => b.to_do?.checked);
     let awaitUncheck = [];
     for (let toDo of toDoList) {
         awaitUncheck.push(
@@ -105,7 +138,17 @@ async function resetDailyGoals() {
 }
 
 async function generateDailyReport() {
-    await resetDailyGoals();
+    const pageId = process.env.GOALS_ID;
+    const content = await notion.blocks.children.list({
+        block_id: pageId
+    });
+    const today = new Date();
+    if (today.getDate() === 1) {
+        let reportsList = content.results.filter(e => e.type === "child_database");
+        await createMonthlyReportPage(reportsList.id);
+    }
+    //console.log(content);
+    await resetDailyGoals(content);
 }
 
 module.exports = {
